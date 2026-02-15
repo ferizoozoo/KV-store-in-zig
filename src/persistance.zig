f62662d86d1bee5acb355f27ffedde6de0715a24
ferizoozoo <farhadzohoorz@gmail.com>
2026-02-14 22:47:42 -0500

    fix: remove bugs when flushing

--- src/persistance.zig
+++ src/persistance.zig
@@ -6,7 +6,7 @@ const KVStore = @import("store.zig").KVStore;
 
 const filename = "data.db";
 
-pub fn get_record(offset: u64, length: usize, allocator: std.mem.Allocator) ![]const u8 {
+pub fn get_record(offset: usize, length: usize, allocator: std.mem.Allocator) ![]const u8 {
     const file = try std.fs.cwd().openFile(filename, .{ .mode = .read_only });
     defer file.close();
 
@@ -17,25 +17,25 @@ pub fn get_record(offset: u64, length: usize, allocator: std.mem.Allocator) ![]c
     return buffer.items;
 }
 
-pub fn flush(buffer: std.StringArrayHashMap([]const u8)) !void {
-    var file: std.fs.File = undefined;
-    const fileExists = try std.fs.cwd().access(filename, .{ .read = true });
-    if (!fileExists) {
-        file = try std.fs.cwd().createFile(filename, .{ .truncate = true });
-    }
-    file = try std.fs.cwd().openFile(filename, .{
+pub fn flush(buffer: *std.StringArrayHashMap([]const u8), main_index: *std.StringArrayHashMap(usize)) !void {
+    var file: std.fs.File = try std.fs.cwd().openFile(filename, .{
         .mode = .read_write,
     });
     defer file.close();
 
     try file.seekFromEnd(0);
 
-    for (buffer.items()) |entry| {
-        const key = entry.key;
-        const value = entry.value;
+    var bufIt = buffer.iterator();
+
+    while (bufIt.next()) |entry| {
+        const key = entry.key_ptr.*;
+        const value = entry.value_ptr.*;
         try file.writeAll(key);
         try file.writeAll(" ");
         try file.writeAll(value);
         try file.writeAll("\n");
+
+        const offset = @as(usize, try file.getEndPos()) - (key.len + 1 + value.len + 1);
+        try main_index.*.put(key, offset);
     }
 }
--- src/store.zig
+++ src/store.zig
@@ -10,8 +10,6 @@ pub const HashTableError = error{
     InvalidKey,
 };
 
-// TODO: The active_buffer can be used for batching writes before flushing to main_index
-// TODO: The main_index should use pointers to values stored in persistance layer
 pub const KVStore = struct {
     size: usize = 256,
     capacity: usize = 16,
@@ -20,30 +18,33 @@ pub const KVStore = struct {
 
     pub fn new(allocator: std.mem.Allocator) !*KVStore {
         const self = try allocator.create(KVStore);
-        const table = std.StringArrayHashMap(usize).init(allocator);
-        const buffer = std.StringArrayHashMap([]const u8).init(allocator);
 
         self.* = .{
-            .main_index = table,
-            .active_buffer = buffer,
+            .main_index = std.StringArrayHashMap(usize).init(allocator),
+            .active_buffer = std.StringArrayHashMap([]const u8).init(allocator),
         };
         return self;
     }
 
     pub fn insert(self: *KVStore, key: []const u8, value: []const u8) !void {
+        if (self.active_buffer.capacity() == self.active_buffer.count()) {
+            self.flush() catch |err| {
+                return err;
+            };
+        }
+
         // NOTE: Duplicating key and value to ensure they are owned by the store
         const k = try self.active_buffer.allocator.dupe(u8, key);
         const v = try self.active_buffer.allocator.dupe(u8, value);
+
+        // TODO: WAL must be done here before changing the data structure
         try self.active_buffer.put(k, v);
     }
 
     pub fn get(self: *KVStore, key: []const u8, allocator: std.mem.Allocator) !?[]const u8 {
         var value = self.active_buffer.get(key);
         if (value == null) {
-            const offset = switch (self.main_index.get(key)) {
-                null => return null,
-                else => |o| o,
-            };
+            const offset = self.main_index.get(key) orelse return null;
             value = try persistance.get_record(offset, length, allocator);
         }
         return value;
@@ -60,6 +61,6 @@ pub const KVStore = struct {
     }
 
     pub fn flush(self: *KVStore) !void {
-        try persistance.flush(self.active_buffer);
+        try persistance.flush(&self.active_buffer, &self.main_index);
     }
 };
