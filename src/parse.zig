dd6dcae5843e569422105497eb2ff6e434b3cfc1
ferizoozoo <farhadzohoorz@gmail.com>
2026-03-01 22:31:28 -0500

    ref: copy the code into the snapshot function in persistance

--- src/parse.zig
+++ src/parse.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const store = @import("store.zig");
+const persistance = @import("persistance.zig");
 
 pub fn parse_request(s: *store.KVStore, request: []const u8) !void {
     if (request.len <= 2) {
@@ -58,33 +59,7 @@ fn parse_snapshot_request(s: *store.KVStore) !void {
     try s.flush();
     std.debug.print("Flushed active buffer\n", .{});
 
-    const source_file_path = "data.db";
-    const destination_file_path = try std.fmt.allocPrint(std.heap.page_allocator, "{}.snap", .{
-        std.time.timestamp(),
-    });
-    defer std.heap.page_allocator.free(destination_file_path);
-
-    const cwd = std.fs.cwd();
-    // ensure the snapshots directory exists (ignore error if it already does)
-    cwd.makeDir("snapshots") catch |err| {
-        if (err == error.PathAlreadyExists) {
-            // This is fine! The folder is already there.
-        } else {
-            // Something actually went wrong (e.g., PermissionDenied)
-            std.debug.print("Failed to create snapshots directory: {}\n", .{err});
-            return err;
-        }
-    };
-
-    var snapshots_dir = try cwd.openDir("snapshots", .{});
-    defer snapshots_dir.close();
-
-    cwd.copyFile(source_file_path, snapshots_dir, destination_file_path, .{}) catch |err| {
-        if (err == error.FileNotFound) {
-            std.debug.print("No data file to snapshot yet (data.db not found)\n", .{});
-        } else {
-            std.debug.print("Failed to create snapshot: {s} (error: {})\n", .{ destination_file_path, err });
-        }
-    };
-    std.debug.print("Snapshot request completed\n", .{});
+    // Create a snapshot of the current state
+    try persistance.snapshot();
+    std.debug.print("Created snapshot\n", .{});
 }
--- src/persistance.zig
+++ src/persistance.zig
@@ -38,19 +38,34 @@ pub fn flush(buffer: *std.StringArrayHashMap([]const u8), main_index: *std.Strin
     }
 }
 
-pub fn snapshot() void {
-    // NOTE: read all records and turn them into a log file (naive implementation, not good)
-    // TODO: refactoring by just pass an allocator into the snapshot() function
-    // TODO: the transaction should be atomic
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
-    const destination_file = try std.fmt.allocPrint(allocator, "{}.snap", .{
-        .{std.time.timestamp()},
+pub fn snapshot() !void {
+    const source_file_path = "data.db";
+    const destination_file_path = try std.fmt.allocPrint(std.heap.page_allocator, "{}.snap", .{
+        std.time.timestamp(),
     });
-    defer allocator.free(destination_file);
+    defer std.heap.page_allocator.free(destination_file_path);
 
-    std.fs.cwd().copyFile(filename, "snapshots", destination_file) catch {
-        std.debug.print("Failed to create snapshot: {s}\n", .{destination_file});
+    const cwd = std.fs.cwd();
+    // ensure the snapshots directory exists (ignore error if it already does)
+    cwd.makeDir("snapshots") catch |err| {
+        if (err == error.PathAlreadyExists) {
+            // This is fine! The folder is already there.
+        } else {
+            // Something actually went wrong (e.g., PermissionDenied)
+            std.debug.print("Failed to create snapshots directory: {}\n", .{err});
+            return err;
+        }
     };
+
+    var snapshots_dir = try cwd.openDir("snapshots", .{});
+    defer snapshots_dir.close();
+
+    cwd.copyFile(source_file_path, snapshots_dir, destination_file_path, .{}) catch |err| {
+        if (err == error.FileNotFound) {
+            std.debug.print("No data file to snapshot yet (data.db not found)\n", .{});
+        } else {
+            std.debug.print("Failed to create snapshot: {s} (error: {})\n", .{ destination_file_path, err });
+        }
+    };
+    std.debug.print("Snapshot request completed\n", .{});
 }
